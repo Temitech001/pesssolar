0bfe51b2f22449107ba500af22e6c54e83b17a7a
monsuru samuel <olaoluwasamuel119@gmail.com>
2024-01-04 08:44:01 -0800

    new

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
   const history = useNavigate();
   function handleSubmit(e) {
     e.preventDefault();
-    if (email === 'Influxsolarinc@gmail.com' && password === 'influxinc000!') {
+    if (email === 'Influxsolarinc@gmail.com' && password === 'influxinc0001!') {
       // Successful login, navigate to the home page
       history('/home');
       setEmail('');
